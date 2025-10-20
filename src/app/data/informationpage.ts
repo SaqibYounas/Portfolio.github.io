25a20f7982a97000cb0788a1dc033850c98ff124
Saqib Younas <muhammadsaqibyounas11@gmail.com>
2025-10-21 04:25:33 +0500

    Add Project-2

---
+++ src/app/data/informationpage.ts
@@ -0,0 +1,13 @@
+// data/information.ts
+import { Project } from '../types/projectgrid';
+
+export const informationProjects: Project[] = [
+  {
+    title: 'Information Display Page',
+    description:
+      'Tech Stack: React.js, Bootstrap 5. Focused on clean layout design, reusable component structure, and full responsiveness. Used Vite for fast build and optimized performance.',
+    image: '/information-1.PNG',
+    detailsPage: '/projects/information-page',
+    liveDemo: 'https://saqibyounas.github.io/mrbigbullofficial.github.io',
+  },
+];
