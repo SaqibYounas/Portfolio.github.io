a78760e727297d9fd2434c10cafdaef7690ed9f2
Saqib Younas <muhammadsaqibyounas11@gmail.com>
2025-10-26 03:06:06 +0500

    Add CI/CD Pipeline

--- jest.config.mjs
+++ jest.config.mjs
@@ -4,9 +4,13 @@ const createJestConfig = nextJest({
   dir: './', 
 });
 
-const customJestConfig = {
+module.exports = {
   testEnvironment: 'jsdom',
+  moduleFileExtensions: ['js', 'jsx', 'ts', 'tsx'],
+  testPathIgnorePatterns: ['/node_modules/', '/.next/'],
   setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+  collectCoverage: true,
+  coverageDirectory: 'coverage',
 };
 
 export default createJestConfig(customJestConfig);
